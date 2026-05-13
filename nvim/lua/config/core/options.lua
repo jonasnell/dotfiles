63deeffc4f796a97565c1e04858e01eee7757317
Jonas Nell <107617226+jonasnell@users.noreply.github.com>
2026-05-13 17:58:59 +0200

    Hide '~' symbol on empty lines

--- nvim/lua/config/core/options.lua
+++ nvim/lua/config/core/options.lua
@@ -55,3 +55,6 @@ vim.o.scrolloff = 10
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 vim.o.confirm = true
+
+-- Don't show ~ symbol for emtpy lines
+vim.o.fillchars = 'eob: '
